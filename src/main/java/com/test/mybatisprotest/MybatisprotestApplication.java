ccd421f66ee7f40d4630f44fb39ae680f7be68a9
jdassd <jdassd@163.com>
2020-04-18 12:50:12 +0800

    Initial commit

---
+++ src/main/java/com/test/mybatisprotest/MybatisprotestApplication.java
@@ -0,0 +1,13 @@
+package com.test.mybatisprotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisprotestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisprotestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/mybatisprotest/MybatisprotestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.mybatisprotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisprotestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
