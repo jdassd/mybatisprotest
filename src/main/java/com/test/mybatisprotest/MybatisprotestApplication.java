58be29e19a6a7c496c825686be2e0c48779b9b3c
jdassd <33302662+jdassd@users.noreply.github.com>
2021-01-29 18:09:12 +0800

    Update MybatisprotestApplication.java

--- src/main/java/com/test/mybatisprotest/MybatisprotestApplication.java
+++ src/main/java/com/test/mybatisprotest/MybatisprotestApplication.java
@@ -3,6 +3,7 @@ package com.test.mybatisprotest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class MybatisprotestApplication {
 
